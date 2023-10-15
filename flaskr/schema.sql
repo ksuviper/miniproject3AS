d5a3a0df42c706173f5a7db6c64963ea246a1d32
ksuviper <ksuviper@gmail.com>
2023-10-15 00:11:48 -0500

    update DB schema to add displayname and unique potency

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,10 +1,12 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS remedy;
+DROP TABLE IF EXISTS remedy_potency;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  password TEXT NOT NULL,
+  displayname TEXT NOT NULL DEFAULT ''
 );
 
 CREATE TABLE remedy (
@@ -20,6 +22,15 @@ CREATE TABLE remedy (
 
 CREATE TABLE remedy_potency (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  potency TEXT NOT NULL
+  potency TEXT NOT NULL UNIQUE
 );
+
+INSERT INTO remedy_potency (potency) VALUES ('3C');
+INSERT INTO remedy_potency (potency) VALUES ('6C');
+INSERT INTO remedy_potency (potency) VALUES ('12C');
+INSERT INTO remedy_potency (potency) VALUES ('30C');
+INSERT INTO remedy_potency (potency) VALUES ('200C');
+INSERT INTO remedy_potency (potency) VALUES ('1M');
+INSERT INTO remedy_potency (potency) VALUES ('10M');
+INSERT INTO remedy_potency (potency) VALUES ('50M');
 
