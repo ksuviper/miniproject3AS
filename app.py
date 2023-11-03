daebdd56f880b3c0f04b5186353794ea00b4b961
ksuviper <ksuviper@gmail.com>
2023-11-02 22:23:55 -0500

    Added class project info to app.py.

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+# INF601 - Advanced Programming in Python
+# Adam Staggenborg
+# Mini Project 3
 
 # x(5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # x(5/5 points) Proper import of packages used.
@@ -23,3 +14,18 @@ if __name__ == '__main__':
 # x(5/5 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # x(5/5 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 # x(10/10 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations. You will need to explain the steps of initializing the database and then how to run the development server for your project.
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run()
+
